0c9d37c37fe29a33f6073810a85eacc763a63468
Eric Sim <simwenxingeric@gmail.com>
2021-10-01 01:05:15 +0800

    First upload!

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const views = require('./controller/views.js')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+views(app)
+
+app.listen(port, () => {
+    console.log(`App is running at port ${port}`)
+})
---
+++ views/js/weather-query.js
@@ -0,0 +1,22 @@
+$(function(){
+    $.ajax({
+        method: "GET",
+        url: "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
+        })
+    .done((data) => {
+        for (let i = 0; i < data['area_metadata'].length; ++i) {
+            $('#location-menu').append(`<option value="${data['area_metadata'][i]['name']}">${data['area_metadata'][i]['name']}</option>`)
+        }
+        $('select[name="location-menu"]').change((event) => {
+            event.preventDefault();
+            let forecast = ""
+            for (let i = 0; i < data['items'][0]['forecasts'].length; ++i) {
+                if ($("#location-menu option:selected").text() == data['items'][0]['forecasts'][i]['area']) {
+                    forecast = data['items'][0]['forecasts'][i]['forecast']
+                }
+            }
+            $("#forecast").html(`${forecast}`)
+        })
+    })
+
+})
