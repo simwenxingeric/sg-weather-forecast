fe17229c3cfc2f5c3d692ab73c15cef7deb54a30
Eric Sim <simwenxingeric@gmail.com>
2021-10-01 13:23:30 +0800

    Weather forecast!

--- views/js/weather-query.js
+++ views/js/weather-query.js
@@ -7,15 +7,19 @@ $(function(){
         for (let i = 0; i < data['area_metadata'].length; ++i) {
             $('#location-menu').append(`<option value="${data['area_metadata'][i]['name']}">${data['area_metadata'][i]['name']}</option>`)
         }
-        $('select[name="location-menu"]').change((event) => {
-            event.preventDefault();
+        let getWeatherData = () => {
             let forecast = ""
             for (let i = 0; i < data['items'][0]['forecasts'].length; ++i) {
                 if ($("#location-menu option:selected").text() == data['items'][0]['forecasts'][i]['area']) {
                     forecast = data['items'][0]['forecasts'][i]['forecast']
                 }
             }
-            $("#forecast").html(`${forecast}`)
+            return $("#forecast").html(`${forecast}`)
+        }
+        getWeatherData()
+        $('select[name="location-menu"]').change((event) => {
+            event.preventDefault();
+            getWeatherData()
         })
     })
 
