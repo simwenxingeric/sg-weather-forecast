646f4348fc3eb3e95addd99a3ecb4b4305b4274d
Eric Sim <simwenxingeric@gmail.com>
2021-10-03 18:04:11 +0800

    Error handling!

--- views/js/weather-query.js
+++ views/js/weather-query.js
@@ -4,23 +4,28 @@ $(function(){
         url: "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
         })
     .done((data) => {
-        for (let i = 0; i < data['area_metadata'].length; ++i) {
-            $('#location-menu').append(`<option value="${data['area_metadata'][i]['name']}">${data['area_metadata'][i]['name']}</option>`)
-        }
-        let getWeatherData = () => {
-            let forecast = ""
-            for (let i = 0; i < data['items'][0]['forecasts'].length; ++i) {
-                if ($("#location-menu option:selected").text() == data['items'][0]['forecasts'][i]['area']) {
-                    forecast = data['items'][0]['forecasts'][i]['forecast']
-                }
+        if (data || Object.keys(data['area_metadata']).length === 0 || Object.keys(data['items']).length === 0) {
+            $('#location-menu').append(`<option value="-">-</option>`)
+            console.error("API is down!")
+        } else {
+            for (let i = 0; i < data['area_metadata'].length; ++i) {
+                $('#location-menu').append(`<option value="${data['area_metadata'][i]['name']}">${data['area_metadata'][i]['name']}</option>`)
             }
-            return $("#forecast").html(`${forecast}`)
-        }
-        getWeatherData()
-        $('select[name="location-menu"]').change((event) => {
-            event.preventDefault();
+            let getWeatherData = () => {
+                let forecast = ""
+                for (let i = 0; i < data['items'][0]['forecasts'].length; ++i) {
+                    if ($("#location-menu option:selected").text() == data['items'][0]['forecasts'][i]['area']) {
+                        forecast = data['items'][0]['forecasts'][i]['forecast']
+                    }
+                }
+                return $("#forecast").html(`${forecast}`)
+            }
             getWeatherData()
-        })
+            $('select[name="location-menu"]').change((event) => {
+                event.preventDefault();
+                getWeatherData()
+            })
+        }
     })
 
 })
