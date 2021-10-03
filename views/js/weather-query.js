3a0e326f8fded2ae200dc12bcb4d9a23bed0e535
Eric Sim <simwenxingeric@gmail.com>
2021-10-03 18:29:52 +0800

    Error handlers.

--- views/js/weather-query.js
+++ views/js/weather-query.js
@@ -4,26 +4,32 @@ $(function(){
         url: "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
         })
     .done((data) => {
+        let forecast = ""
+
         if (data || Object.keys(data['area_metadata']).length === 0 || Object.keys(data['items']).length === 0) {
             $('#location-menu').append(`<option value="-">-</option>`)
-        } else {
+            forecast = "No forecast found."
+            $("#forecast").text(`${forecast}`)
+        } else { 
             for (let i = 0; i < data['area_metadata'].length; ++i) {
                 $('#location-menu').append(`<option value="${data['area_metadata'][i]['name']}">${data['area_metadata'][i]['name']}</option>`)
             }
-            let getWeatherData = () => {
-                let forecast = ""
+
+            let getWeatherForecast = () => {
                 for (let i = 0; i < data['items'][0]['forecasts'].length; ++i) {
                     if ($("#location-menu option:selected").text() == data['items'][0]['forecasts'][i]['area']) {
                         forecast = data['items'][0]['forecasts'][i]['forecast']
                     }
                 }
-                return $("#forecast").html(`${forecast}`)
             }
-            getWeatherData()
+            getWeatherForecast()
+            $("#forecast").text(`${forecast}`)
+
             $('select[name="location-menu"]').change((event) => {
                 event.preventDefault();
-                getWeatherData()
-            })
+                getWeatherForecast()
+                $("#forecast").text(`${forecast}`)
+            })   
         }
     })
 
